778d63926b180bcbf929a5e1835854aae8caef74
Elias Galvão Alves <galvaoalveselias@gmail.com>
2024-04-23 15:07:06 -0300

    scrapping thais imobiliaria

--- airbnb/scrapping_airbnb.py
+++ airbnb/scrapping_airbnb.py
@@ -1,26 +1,58 @@
+import pandas as pd
 from time import sleep
-import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import NoSuchElementException
 
-driver_path = ChromeDriverManager().install()
+opts = Options()
+opts.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36")
+# opts.add_argument("--headless")
 
-# Create a service object with the driver path
-service = Service(driver_path)
+driver = webdriver.Chrome(
+    service=Service(ChromeDriverManager().install()),
+    options=opts
+)
 
-navegador = webdriver.Chrome(service=service)
+driver.get('https://www.airbnb.com.br/s/Distrito-Federal--Brasil/homes?tab_id=home_tab&refinement_paths%5B%5D=%2Fhomes&flexible_trip_lengths%5B%5D=one_week&monthly_start_date=2024-05-01&monthly_length=3&monthly_end_date=2024-08-01&price_filter_input_type=0&channel=EXPLORE&query=Distrito%20Federal%2C%20Brasil&place_id=ChIJ1wSIEPI6WpMRVlAUyZAjuj4&date_picker_type=calendar&source=structured_search_input_header&search_type=filter_change&price_filter_num_nights=5')
 
-navegador.get('https://www.airbnb.com.br/s/Brasilia--Bras%C3%ADlia-~-Federal-District--Brazil/homes?tab_id=home_tab&refinement_paths%5B%5D=%2Fhomes&flexible_trip_lengths%5B%5D=one_week&monthly_start_date=2024-04-01&monthly_length=3&monthly_end_date=2024-07-01&price_filter_input_type=0&channel=EXPLORE&query=Brasilia%2C%20Bras%C3%ADlia%20-%20Federal%20District&place_id=ChIJMY_byXY3WpMRrGc50eIQKSk&date_picker_type=calendar&source=structured_search_input_header&search_type=autocomplete_click')
+while True:
+    page_content = driver.page_source
+    site = BeautifulSoup(page_content, 'html.parser')
+    imoveis = site.findAll('div', attrs={'data-testid': 'card-container'})
 
-sleep(2)
+    lista_de_imoveis = []
 
-input_place = navegador.find_element(By.CSS_SELECTOR, 'a')
-print(input_place['href'])
+    for imovel in imoveis:
+        titulo = imovel.find('div', attrs={'data-testid': 'listing-card-title'})
+        titulo_text = titulo.text.strip() if titulo else None
+        
+        subtitulo = imovel.find('span', attrs={'data-testid': 'listing-card-name'})
+        subtitulo_text = subtitulo.text.strip() if subtitulo else None
 
-navegador.quit()
+        link = 'https://www.airbnb.com.br/' + imovel.find('a')['href']
+        
+        tipo = imovel.find('p', attrs={'class': 'card_split_vertically__type'})
+        tipo_text = tipo.text.strip() if tipo else None
 
+        preco = imovel.find('span', attrs={'class': 'a8jt5op atm_3f_idpfg4 atm_7h_hxbz6r atm_7i_ysn8ba atm_e2_t94yts atm_ks_zryt35 atm_l8_idpfg4 atm_mk_stnw88 atm_vv_1q9ccgz atm_vy_t94yts dir dir-ltr'})
 
+        lista_de_imoveis.append([titulo_text, subtitulo_text, link, preco])
 
+    df_imovel = pd.DataFrame(lista_de_imoveis, columns=['Título', 'Subtitulo', 'Link', 'Preço'])
+    df_imovel.to_excel(r'C:\Users\galva\OneDrive\Documentos\GitHub\web-scrapping-com-python\thais_imobiliaria\imoveis_scrapping_thais_imobiliaria_venda.xlsx', index=False)
+    print(df_imovel)
+
+    try:
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '[aria-label=Próximo]'))).click()
+        sleep(2)  # Aguarde o carregamento da próxima página
+    except TimeoutException:
+        break  # Se não houver mais botão "Próximo", saia do loop
+
+driver.quit()
--- thais_imobiliaria/thais_scrapping.py
+++ thais_imobiliaria/thais_scrapping.py
@@ -1,102 +1,3 @@
-# import requests
-# import pandas as pd
-# from time import sleep
-# from bs4 import BeautifulSoup
-# from selenium import webdriver
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.chrome.service import Service
-# from webdriver_manager.chrome import ChromeDriverManager
-# from selenium.webdriver.chrome.service import Service as ChromeService
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
-# from selenium.common.exceptions import NoSuchElementException
-
-# navegador = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-
-# # Create a service object with the driver path
-
-# navegador.get('https://www.thaisimobiliaria.com.br/imoveis/a-venda')
-
-# sleep(2)
-# while True:
-#     try:
-#         # Espera até que o elemento dos cookies desapareça
-#         WebDriverWait(navegador, 20).until(EC.invisibility_of_element_located((By.ID, "cookies-component")))
-#         # Verifica se o botão "Ver Mais" está presente na página
-#         botao_ver_mais = navegador.find_element(By.CSS_SELECTOR, '.btn.btn-md.btn-primary.btn-next')
-        
-#         # Se o botão estiver presente, clique nele
-#         botao_ver_mais.click()
-        
-#         # Pausa temporária para aguardar o carregamento da próxima página
-#         sleep(10)  # Ajuste o tempo conforme necessário
-        
-#     except NoSuchElementException:
-#         # Se o botão não estiver mais presente, saia do loop
-#         break
-
-# page_content = navegador.page_source
-
-# site = BeautifulSoup(page_content, 'html.parser')
-# imoveis = site.findAll('a', attrs={'class': 'card_split_vertically borderHover'})
-
-# lista_de_imoveis = []
-
-
-# for imovel in imoveis:
-#         # Título do imóvel
-#         titulo = imovel.find('h2', attrs={'class': 'card_split_vertically__location'})
-#         titulo_text = titulo.text.strip() if titulo else None
-
-#         # Link do imovel
-#         link = 'https://www.thaisimobiliaria.com.br' + imovel['href']
-        
-#         # Tipo do imóvel
-#         tipo = imovel.find('p', attrs={'class': 'card_split_vertically__type'})
-#         tipo_text = tipo.text.strip() if tipo else None
-
-#         # Preco aluguel
-#         preco = None
-#         preco_area = imovel.find('div', attrs={'class': 'card_split_vertically__value-container'})
-#         if preco_area:
-#                 preco = preco_area.find('p', attrs={'class': 'card_split_vertically__value'})
-#                 if preco:
-#                         preco = preco.text.strip()
-#                 else:
-#                         preco = "Preço não especificado"
-#         else:
-#                 preco = "Preço não especificado"
-
-
-#         # Metro quadrado
-#         metro = imovel.find('li', attrs={'class': 'card_split_vertically__spec'})
-#         metro_text = metro.text.replace('m²', '').strip() if metro else None
-
-
-#         # quartos, suíte, vagas
-#         quarto_suite_vaga = imovel.find('ul', attrs={'class': 'card_split_vertically__specs'})
-#         quarto_suite_vaga_lista = quarto_suite_vaga.text.split()
-#         # quarto = quarto_suite_vaga.text.replace('m²', '')[1:2]
-#         # suite = quarto_suite_vaga.text.replace('m²', '')[2:3]
-#         # banheiro = quarto_suite_vaga.text.replace('m²', '')[3:4]
-#         # vaga = quarto_suite_vaga.text.replace('m²', '')[4:5]
-#         # quarto_text = quarto.strip() if quarto else None
-#         # suite_text = suite.strip() if suite else None
-#         # vaga_text = vaga.strip() if vaga else None
-        
-
-#         lista_de_imoveis.append([titulo_text, tipo_text, link, preco, metro_text, quarto_suite_vaga_lista])
-
-
-# df_imovel = pd.DataFrame(lista_de_imoveis, columns=['Título', 'Tipo', 'Link', 'Preço','Metro Quadrado', 'Quarto, Suite, Banheiro, Vaga'])
-
-# df_imovel.to_excel(r'C:\Users\galva\OneDrive\Documentos\GitHub\web-scrapping-com-python\thais_imobiliaria\imoveis_scrapping_thais_imobiliaria_venda.xlsx', index=False)
-
-# print(df_imovel)
-# navegador.quit()
-
-
-
 import requests
 import pandas as pd
 from time import sleep
@@ -109,36 +10,62 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.chrome.options import Options
 
-navegador = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
-# Create a service object with the driver path
+opts = Options()
+opts.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36")
+#opts.add_argument("--headless")
 
-navegador.get('https://www.thaisimobiliaria.com.br/imoveis/a-venda?pagina=1')
+# Alternativamente:
+# driver = webdriver.Chrome(
+#     service=Service('./chromedriver'),
+#     options=opts
+# )
+
+driver = webdriver.Chrome(
+    service=Service(ChromeDriverManager().install()),
+    options=opts
+)
+
+driver.get('https://www.thaisimobiliaria.com.br/imoveis/a-venda')
+
+sleep(3)
+
+page_content = driver.page_source
+
+site = BeautifulSoup(page_content, 'html.parser')
+
+imoveis = site.findAll('a', attrs={'class': 'card_split_vertically borderHover'})
+
 
 sleep(2)
+while True:
+    try:
+        # Espera até que o elemento dos cookies desapareça
+        WebDriverWait(driver, 20).until(EC.invisibility_of_element_located((By.ID, "cookies-component")))
+        # Verifica se o botão "Ver Mais" está presente na página
+        botao_ver_mais = driver.find_element(By.CSS_SELECTOR, '.btn.btn-md.btn-primary.btn-next')
+        
+        # Se o botão estiver presente, clique nele
+        botao_ver_mais.click()
+        
+        # Pausa temporária para aguardar o carregamento da próxima página
+        sleep(1)  # Ajuste o tempo conforme necessário
+        
+    except NoSuchElementException:
+        # Se o botão não estiver mais presente, saia do loop
+        break
 
+page_content = driver.page_source
 
-
-
+site = BeautifulSoup(page_content, 'html.parser')
+imoveis = site.findAll('a', attrs={'class': 'card_split_vertically borderHover'})
 
 lista_de_imoveis = []
-pagina = 1
-
-for pagina in range(309):
-    pagina += 1
-    resposta = requests.get(f'https://www.thaisimobiliaria.com.br/imoveis/a-venda?pagina={pagina}')
-
-    conteudo = resposta.content
 
 
-    page_content = navegador.page_source
-    site = BeautifulSoup(page_content, 'html.parser')
-
-    # HTML do anúncio do imóvel
-    imoveis = site.findAll('a', attrs={'class': 'card_split_vertically borderHover'})
-
-    for imovel in imoveis:
+for imovel in imoveis:
         # Título do imóvel
         titulo = imovel.find('h2', attrs={'class': 'card_split_vertically__location'})
         titulo_text = titulo.text.strip() if titulo else None
@@ -177,17 +104,15 @@ for pagina in range(309):
         # vaga = quarto_suite_vaga.text.replace('m²', '')[4:5]
         # quarto_text = quarto.strip() if quarto else None
         # suite_text = suite.strip() if suite else None
-        # banheiro_text = banheiro.strip() if banheiro else None
         # vaga_text = vaga.strip() if vaga else None
-        
         
 
         lista_de_imoveis.append([titulo_text, tipo_text, link, preco, metro_text, quarto_suite_vaga_lista])
 
 
-df_imovel = pd.DataFrame(lista_de_imoveis, columns=['Título', 'Tipo', 'Link', 'Preço','Metro Quadrado', 'Quarto...'])
+df_imovel = pd.DataFrame(lista_de_imoveis, columns=['Título', 'Tipo', 'Link', 'Preço','Metro Quadrado', 'Quarto, Suite, Banheiro, Vaga'])
 
 df_imovel.to_excel(r'C:\Users\galva\OneDrive\Documentos\GitHub\web-scrapping-com-python\thais_imobiliaria\imoveis_scrapping_thais_imobiliaria_venda.xlsx', index=False)
 
 print(df_imovel)
-navegador.quit()
+
